817cca628e79a2786ba4329fd06dac6f7b654464
CongyuanWang <38725802+cy-wang15@users.noreply.github.com>
2022-03-14 21:18:47 +0800

    change some code

--- pyfeng/HestionMcAe2.py
+++ pyfeng/HestionMcAe2.py
@@ -11,7 +11,6 @@ from pyfeng import sv_abc as sv
 class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
     """
     Almost exact MC for Heston model.
-
     Underlying price is assumed to follow a geometric Brownian motion.
     Volatility (variance) of the price is assumed to follow a CIR process.
     Example:
@@ -31,7 +30,6 @@ class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
     def set_mc_params(self, n_path=10000, rn_seed=None, antithetic=True, dist=0):
         """
         Set MC parameters
-
         Args:
             n_path: number of paths
             rn_seed: random number seed
@@ -52,7 +50,6 @@ class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
     def chi_dim(self):
         """
         Noncentral Chi-square (NCX) distribution's degree of freedom
-
         Returns:
             degree of freedom (scalar)
         """
@@ -62,7 +59,6 @@ class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
     def chi_lambda(self, texp):
         """
         Noncentral Chi-square (NCX) distribution's noncentrality parameter
-
         Returns:
             noncentrality parameter (scalar)
         """
@@ -101,16 +97,10 @@ class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
 
 #First define r(s) and its differention
     def r(self,s):
-        result = np.sqrt(self.mr ** 2 + 2 * s * self.vov ** 2)
-        return result
-
-    def dr(self,s):
-        result = self.vov**2 / self.r(s)
-        return result
-    
-    def d2r(self,s):
-        result = - self.vov**4/ self.r(s)**3
-        return result
+        r = np.sqrt(self.mr ** 2 + 2 * s * self.vov ** 2)
+        dr = self.vov**2 / r
+        d2r = - self.vov**4/ r**3
+        return r,dr,d2r
     
     def u(self,x,texp):
         result = 0.5*x*texp
@@ -123,146 +113,76 @@ class HestonMcAe2(sv.SvABC, sv.CondMcBsmABC):
     def d2u(self,x,texp):
         return 0
     
-#define subfunctions: A,f,g
+#define subfunctions: A
     def A(self,u):
         result = u/np.sinh(u)
         return result
-    
-    def f(self,u,texp,var_t):
-        cof = 2*(self.sigma + var_t )/ self.vov ** 2/texp
-        result = cof  *  np.cosh(u) * self.A(u)
-        return result
-
-    def g(self,u,texp,var_t):
-        cog = 4 * np.sqrt(self.sigma * var_t )/self.vov ** 2/texp
-        result = cog *  self.A(u)
-        return result
-#define subfunctions
-    def fai(self,u,texp,var_t):
-        nu = self.nu()
-        f = self.f(u,texp,var_t)
-        g = self.g(u,texp,var_t)
-        I = self.I(nu,g)
-        A = self.A(u)
-        result = I*np.exp(f)*A
-        return result
-#define MGF(-sX):   
-    def MGF(self,s,texp,var_t):
-        u_s = self.u(self.r(s))
-        u_mr = self.u(self.mr)
-        result = self.fai(u_s,texp,var_t)/self.fai(u_mr,texp,var_t)
-        return result 
-    
-    
-    
-#define subfunctions differention:( dA,df,dg )/du
     def dA(self,u):
         result = (-np.cosh(u)*u+np.sinh(u))/np.sinh(u)**2
         return result
-    
-    def df(self,u,texp,var_t):
-        cof = 2*(self.sigma + var_t )/ self.vov ** 2/texp
-        result =cof * ( np.sinh(u)*self.A(u) + np.cosh(u)*self.dA(u) )
-        return result
-
-    def dg(self,u,texp,var_t):
-        cog = 4 * np.sqrt(self.sigma * var_t )/self.vov ** 2/texp
-        result =  cog * self.dA(u)
-        return result
-#define subfunctions:( d2A,d2f,d2g )/du2
     def d2A(self,u):
         result =(- u*np.sinh(u)**2 - 2*np.cosh(u)*np.sinh(u)+2*u*np.cosh(u)**2)/np.sinh(u)**3
         return result
     
-    def d2f(self,u,texp,var_t):
-        cof = 2*(self.sigma + var_t )/ self.vov ** 2/texp
+    def f(self,u,texp,var_t):
         A = self.A(u)
         dA = self.dA(u)
         d2A = self.d2A(u)
-        result = cof * (np.cosh(u)*(A+d2A)+2*np.sinh(u)*dA)
-        return result
+        cof = 2*(self.sigma + var_t )/ self.vov ** 2/texp
+        f = cof  *  np.cosh(u) * A
+        df = cof * ( np.sinh(u)*A + np.cosh(u)*dA )
+        d2f = cof * (np.cosh(u)*(A+d2A)+2*np.sinh(u)*dA)
+        return f,df,d2f
 
-    def d2g(self,u,texp,var_t):
-        cog = 4 * np.sqrt(self.sigma * var_t )/self.vov ** 2/texp
-        result = cog* self.d2A(u)
-        return result
-
-#define subfunctions
-    def fai(self,u,texp,var_t):
-        nu = self.nu()
-        f = self.f(u,texp,var_t)
-        g = self.g(u,texp,var_t)
-        I = self.I(nu,g)
-        A = self.A(u)
-        result = I*np.exp(-f)*A
-        return result
-#define MGF(-sX):   
-    def MGF(self,s,texp,var_t):
-        u_s = self.u(self.r(s))
-        u_mr = self.u(self.mr)
-        result = self.fai(u_s,texp,var_t)/self.fai(u_mr,texp,var_t)
-        return result
-    
-# define dfai_du(first differention)
-    def dfai(self,u,texp,var_t):
-        nu = self.nu()
-        f = self.f(u,texp,var_t)
-        df = self.df(u,texp,var_t)
-        g = self.g(u,texp,var_t)
-        dg = self.dg(u,texp,var_t)
-        I = self.I(nu,g)
-        dI = self.dI(nu,g) * dg
+    def g(self,u,texp,var_t):
         A = self.A(u)
         dA = self.dA(u)
+        d2A = self.d2A(u)
+        cog = 4 * np.sqrt(self.sigma * var_t )/self.vov ** 2/texp
+        g = cog *  A
+        dg = cog * dA
+        d2g = cog * d2A
+        return g,dg,d2g
+
     
-        result = dI*np.exp(-f)*A + I*(-df)*np.exp(-f)*A +I*np.exp(-f)*dA
-        return result
-    
-    def d2fai(self,u,texp,var_t):
+    def fai(self,u,texp,var_t):
         nu = self.nu()
-        f = self.f(u,texp,var_t)
-        df = self.df(u,texp,var_t)
-        d2f = self.d2f(u,texp,var_t)
-        g = self.g(u,texp,var_t)
-        dg = self.dg(u,texp,var_t)
-        d2g = self.d2g(u,texp,var_t)
+        f,df,d2f = self.f(u,texp,var_t)
+        g,dg,d2g = self.g(u,texp,var_t)
         I = self.I(nu,g)
         dI = self.dI(nu,g) * dg
         d2I = self.d2I(nu,g) * dg**2 + self.dI(nu,g) * d2g
         A = self.A(u)
         dA = self.dA(u)
-        d2A = self.d2A(u)
-        
-        result = d2I*np.exp(-f)*A + dI*(-df)*np.exp(-f)*A +dI*np.exp(-f)*dA + \
+        d2A = self.d2A(u)       
+        fai = I*np.exp(-f)*A
+        dfai = dI*np.exp(-f)*A + I*(-df)*np.exp(-f)*A +I*np.exp(-f)*dA
+        d2fai = d2I*np.exp(-f)*A + dI*(-df)*np.exp(-f)*A +dI*np.exp(-f)*dA + \
                 dI*(-df)*np.exp(-f)*A + I*(-d2f+df**2)*np.exp(-f)*A+I*(-df)*np.exp(-f)*dA + \
                 dI*np.exp(-f)*dA+I*(-df)*np.exp(-f)*dA+I*np.exp(-f)*d2A
-        return result
+                
+        return fai,dfai,d2fai
 
 #find M1,M2:
-    def moment_1(self,s,texp,var_t):
-        r_s = self.r(s)
-        u_r = self.u(r_s,texp)
-        u_mr = self.u(self.mr,texp)
-        result =  self.dfai(u_r,texp,var_t) * self.du(r_s,texp) * self.dr(s)/ self.fai(u_mr,texp,var_t)
-        return result
-
-    def moment_2(self,s,texp,var_t):
-        r_s = self.r(s)
-        u_r = self.u(r_s,texp)
-        u_mr = self.u(self.mr,texp)
-        result =(self.d2fai(u_r,texp,var_t) * (self.du(r_s,texp)**2 )* (self.dr(s)**2)+   \
-                 self.dfai(u_r,texp,var_t) * self.d2u(r_s,texp) * (self.dr(s)**2) +      \
-                 self.dfai(u_r,texp,var_t) * self.du(r_s,texp) * self.d2r(s))       \
-                 / self.fai(u_mr,texp,var_t)
-        return result
+    def moment(self,s,texp,var_t):
+        r,dr,d2r= self.r(s)
+        u_r = self.u(r,texp)
+        du_r = self.du(r,texp)
+        u_mr= self.u(self.mr,texp)
+        
+        fai_mr,dfai_mr,d2fai_mr = self.fai(u_mr,texp,var_t)
+        fai_ur,dfai_ur,d2fai_ur = self.fai(u_r,texp,var_t)
+        
+        m1 =   - dfai_ur* du_r * dr/ fai_mr
+        m2 =(d2fai_ur * du_r**2 * dr**2+  dfai_ur * du_r * d2r ) / fai_mr
+        return m1,m2
 
 
     #find value
     def cond_spot_sigma(self, texp):
         var_t = self.var_t(texp)
         rhoc = np.sqrt(1.0 - self.rho ** 2)
-        m1 = - self.moment_1(0,texp,var_t)
-        m2 = self.moment_2(0,texp,var_t)
+        m1 , m2 = self.moment(0, texp, var_t)
                  
         if self.dist == 0:
             # mu and lambda defined in https://en.wikipedia.org/wiki/Inverse_Gaussian_distribution
